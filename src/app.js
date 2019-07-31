836dcbffd9da0bcd838ec164bd19ffffb60f7ffe
NathanPB <nathan.pbombana@gmail.com>
2019-07-31 20:19:43 -0300

    Bro the app is done and I didn't commited anything yet ://

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
+/*
+Copyright 2019 Nathan P. Bombana
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 import express from 'express';
 import path from 'path';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
 import routes from './routes';
+import rateLimit from 'express-rate-limit';
+import { renderFile } from 'ejs';
 
 const app = express();
 app.disable('x-powered-by');
@@ -17,6 +29,12 @@ app.use(logger('dev', {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '../public')));
+
+app.use(rateLimit({
+  windowMs: 10000,
+  max: 2,
+  message: "Heyy! Hold up! I can't process so many fucking requests. Please go away and come back later, ehh"
+}));
 
 // Routes
 app.use('/', routes);
@@ -36,5 +54,8 @@ app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
       message: err.message
     });
 });
+
+app.engine('html', renderFile);
+app.set('view engine', 'html');
 
 export default app;
